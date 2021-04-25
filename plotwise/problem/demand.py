7fffc23f33f1441ce52452b7e0be7f9d7c247c47
freekdehaas <freek.de.haas@1qbit.com>
2021-04-25 14:07:19 -0700

    create classmethod to get from file

--- plotwise/problem/demand.py
+++ plotwise/problem/demand.py
@@ -1,6 +1,8 @@
 from collections import namedtuple
-from typing import Set
+from typing import Set, Iterable
 from dataclasses import dataclass
+from os import path
+
 
 Coordinate = namedtuple("Coordinate", "x y")
 
@@ -9,3 +11,22 @@ Coordinate = namedtuple("Coordinate", "x y")
 class Demand:
     deliveries: Set[Coordinate]
     pickups: Set[Coordinate]
+
+    @classmethod
+    def from_file(cls, file_path: str) -> "Demand":
+        demand_file_loader = DemandFileLoader(file_path=file_path)
+        deliveries = set(demand_file_loader.get_deliveries())
+        pickups = set(demand_file_loader.get_pickups())
+
+        return cls(deliveries=deliveries, pickups=pickups)
+
+
+class DemandFileLoader:
+    def __init__(self, file_path: str):
+        assert path.exists(file_path), f"Can't find file {file_path}"
+
+    def get_deliveries(self) -> Iterable[Coordinate]:
+        raise NotImplementedError
+
+    def get_pickups(self) -> Iterable[Coordinate]:
+        raise NotImplementedError
