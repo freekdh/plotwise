59087be595f28b73731a6214728500495b7eaad8
freekdehaas <freek.de.haas@1qbit.com>
2021-04-25 14:48:05 -0700

    add a demand reader from file

--- plotwise/problem/demand.py
+++ plotwise/problem/demand.py
@@ -2,9 +2,36 @@ from collections import namedtuple
 from typing import Set, Iterable
 from dataclasses import dataclass
 from os import path
-
+from warnings import warn
+import math
+import re
 
 Coordinate = namedtuple("Coordinate", "x y")
+
+
+class DemandFileLoader:
+    def __init__(self, file_path: str, from_line=9):
+        """Load from file the events, starting from line 'from_line'
+
+        Args:
+            file_path (str): The file path with the events
+            from_line (int, optional): Start line to read for events. Defaults to 10.
+        """
+        assert path.exists(file_path), f"Can't find file {file_path}"
+        self._file_path = file_path
+        self._from_line = from_line
+
+    def get_events(self) -> Iterable[Coordinate]:
+        with open(self._file_path) as file:
+            return [
+                self._get_event(line)
+                for i, line in enumerate(file)
+                if i >= self._from_line
+            ]
+
+    def _get_event(self, line: str) -> Coordinate:
+        customer_number, x, y, demand, read_time, due_date, service_time = line.split()
+        return Coordinate(x=int(x), y=int(y))
 
 
 @dataclass(frozen=True, eq=False)
@@ -13,20 +40,22 @@ class Demand:
     pickups: Set[Coordinate]
 
     @classmethod
-    def from_file(cls, file_path: str) -> "Demand":
+    def from_file_50_50(cls, file_path: str) -> "Demand":
+        """Import events from file and split. First 50% of events are deliveries and rest is pickups.
+        If number of events is uneven, n_deliveries == n_pickups + 1
+
+        Returns:
+            Demand
+        """
         demand_file_loader = DemandFileLoader(file_path=file_path)
-        deliveries = set(demand_file_loader.get_deliveries())
-        pickups = set(demand_file_loader.get_pickups())
+        events = list(demand_file_loader.get_events())
+
+        if n_events := len(events):
+            n_deliveries = math.ceil(n_events / 2)
+            deliveries = set(events[:n_deliveries])
+            pickups = set(events[n_deliveries:])
+        else:
+            warn(f"No events found in {file_path}")
+            deliveries, pickups = {}, {}
 
         return cls(deliveries=deliveries, pickups=pickups)
-
-
-class DemandFileLoader:
-    def __init__(self, file_path: str):
-        assert path.exists(file_path), f"Can't find file {file_path}"
-
-    def get_deliveries(self) -> Iterable[Coordinate]:
-        raise NotImplementedError
-
-    def get_pickups(self) -> Iterable[Coordinate]:
-        raise NotImplementedError
--- tests/test_demand.py
+++ tests/test_demand.py
@@ -4,11 +4,29 @@ from plotwise.problem.demand import Demand, Coordinate
 
 
 @pytest.fixture(scope="session")
-def demand():
+def example_demand():
     return Demand(
         deliveries={Coordinate(2, 5), Coordinate(-10, -9)}, pickups={Coordinate(1, 1)}
     )
 
 
-def test_demand(demand):
+def test_demand(example_demand):
     pass
+
+
+@pytest.mark.parametrize(
+    "file_path, expected_n_deliveries",
+    [("data/homberger_200_customer_instances/C1_2_1.TXT", 101)],
+)
+def test_demand_from_file_deliveries(file_path, expected_n_deliveries):
+    demand_from_file = Demand.from_file_50_50(file_path=file_path)
+    assert len(set(demand_from_file.deliveries)) == expected_n_deliveries
+
+
+@pytest.mark.parametrize(
+    "file_path, expected_n_pickups",
+    [("data/homberger_200_customer_instances/C1_2_1.TXT", 100)],
+)
+def test_demand_from_file_pickups(file_path, expected_n_pickups):
+    demand_from_file = Demand.from_file_50_50(file_path)
+    assert len(set(demand_from_file.pickups)) == expected_n_pickups
