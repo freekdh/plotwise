def4cbecad6009c75728777886584253ac48d451
freekdehaas <freek.de.haas@1qbit.com>
2021-04-25 13:37:28 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="plotwise",
+    version="0.1",
+    description="In-process code for plotwise project",
+    author="Freek de Haas",
+    packages=["plotwise"],
+)
