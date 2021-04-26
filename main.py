7ebee0f0552dd49046d0aa861021dddabb85eed2
freekdehaas <freek.de.haas@1qbit.com>
2021-04-25 17:06:44 -0700

    add a simple way to collect the deliveries if the shortest route does not matter

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
-from plotwise.problem.demand import Demand
+from itertools import takewhile
+
+from plotwise.problem.demand import Demand, Event
 from plotwise.problem.vehicle import Vehicle
 from plotwise.problem.environment import ProblemEnvironment
 
 
-def solve_problem(
-    demand: Demand, vehicle: Vehicle, problem_environment: ProblemEnvironment
-):
-    pass
+def get_deliveries_for_vehicle(demand: Demand, vehicle: Vehicle) -> Event:
+    """Sort deliveries by capacity and yield delivery until capacity limit is reached
+
+    Args:
+        demand (Demand)
+        vehicle (Vehicle)
+
+    Yields:
+        Event
+    """
+    sorted_delivery_by_capacity = sorted(
+        (delivery for delivery in demand.deliveries),
+        key=lambda delivery: delivery.capacity,
+    )
+
+    sum_of_capacity = 0
+    for delivery in sorted_delivery_by_capacity:
+        if delivery.capacity + sum_of_capacity <= vehicle.capacity_limit:
+            sum_of_capacity += delivery.capacity
+            yield delivery
+        else:
+            return
 
 
 if __name__ == "__main__":
     demand = Demand.from_file_50_50(
         file_path="data/homberger_200_customer_instances/C1_2_1.TXT"
     )
-    vehicle = Vehicle(capacity_limit=1000)
-    problem_environment = ProblemEnvironment()
+    vehicle = Vehicle(capacity_limit=100)
 
-    solve_problem(demand, vehicle, problem_environment)
+    deliveries = set(get_deliveries_for_vehicle(demand, vehicle))
+
+    print(deliveries)
